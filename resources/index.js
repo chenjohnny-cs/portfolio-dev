d5cb175d543255d8aaa0b1ce30f4459179ce1293
Johnny Chen <chenj67@rpi.edu>
2024-07-23 02:18:51 -0400

    initial commit

---
+++ resources/index.js
@@ -0,0 +1,11 @@
+function dropCurtain() {
+    document.getElementById("curtain-menu").style.height = '45%';
+}
+
+function raiseCurtain() {
+    document.getElementById("curtain-menu").style.height = '0%';
+}
+
+$(document).ready(function() {
+
+})
